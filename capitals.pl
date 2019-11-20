c7fd9a61fd0f5b12a42387c99d334af896a6268d
João <joao.rlima@hotmail.com>
2019-11-20 18:41:43 -0300

    Capitals of Brazil. Standard distance of 1.

---
+++ capitals.pl
@@ -0,0 +1,74 @@
+/* MANAUS */
+capitais(manaus,boa_vista,1).
+capitais(manaus,belem,1).
+capitais(manaus,cuiaba,1).
+capitais(manaus,porto_velho,1).
+capitais(manaus,rio_branco,1).
+/* CUIABA */
+capitais(cuiaba,belem,1).
+capitais(cuiaba,campo_grande,1).
+capitais(cuiaba,goiania,1).
+capitais(cuiaba,araguaina,1).
+/* RIO BRANCO */
+capitais(rio_branco, porto_velho,1).
+/* BOA VISTA */
+capitais(boa_vista,belem,1).
+/* PORTO VELHO */
+capitais(porto_velho,cuiaba,1).
+/* ARAGUAIANA */
+capitais(araguaina,sao_luis,1).
+capitais(araguaina,goiania,1).
+capitais(araguaina,teresina,1).
+capitais(araguaina,salvador,1).
+capitais(araguaina,goiania,1).
+/* BELEM */
+capitais(belem,macapa,1).
+capitais(belem,araguaina,1).
+capitais(belem,sao_luis,1).
+/* BRASILIA / GOIANIA */
+capitais(brasilia,goiania,1).
+capitais(goiania,belo_horizonte,1).
+capitais(goiania,campo_grande,1).
+capitais(goiania,sao_paulo,1).
+capitais(goiania,salvador,1).
+/* CAMPO GRANDE */
+capitais(campo_grande,sao_paulo,1).
+capitais(campo_grande,curitiba,1).
+capitais(campo_grande,belo_horizonte,1).
+/* RIO DE JANEIRO */
+capitais(rio_janeiro,vitoria,1).
+capitais(rio_janeiro,belo_horizonte,1).
+/* SAO PAULO */
+capitais(sao_paulo,curitiba,1).
+capitais(sao_paulo,belo_horizonte,1).
+capitais(sao_paulo,rio_janeiro,1).
+/* VITORIA */
+capitais(vitoria,belo_horizonte,1).
+capitais(vitoria,salvador,1).
+/* BELO HORIZONTE */
+capitais(belo_horizonte,salvador,1).
+/* SALVADOR */
+capitais(salvador,aracaju,1).
+capitais(salvador,teresina,1).
+capitais(salvador,recife,1).
+capitais(salvador,maceio,1).
+/* MACEIO */
+capitais(maceio,aracaju,1).
+capitais(maceio,recife,1).
+/* JOAO PESSOA */
+capitais(joao_pessoa,natal,1).
+capitais(joao_pessoa,fortaleza,1).
+/* RECIFE */
+capitais(recife,joao_pessoa,1).
+capitais(recife,teresina,1).
+capitais(recife,fortaleza,1).
+/* NATAL */
+capitais(natal,fortaleza,1).
+/* FORTALEZA */
+capitais(fortaleza,teresina,1).
+/* TERESINA */
+capitais(teresina,sao_luis,1).
+/* CURITIBA */
+capitais(curitiba,florianopolis,1).
+/* FLORIANOPOLIS */
+capitais(florianopolis,porto_alegre,1).
